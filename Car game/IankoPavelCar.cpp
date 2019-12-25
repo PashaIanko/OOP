a05164645ad510f9963d5d27658c9cbaa9a00ec8
Pavel <yanpavevg@yandex.ru>
2019-12-26 00:17:52 +0700

    Initial commit

--- Car game/IankoPavelCar.cpp
+++ Car game/IankoPavelCar.cpp
@@ -1,5 +1,30 @@
 #include "IankoPavelCar.h"
 
+void IankoCar::set_body_type(b2BodyType type_) {
+	body_descriptor.type = type_;
+}
+
+void IankoCar::set_wheel_type(b2BodyType type_) {
+	wheel_descriptor.type = type_;
+}
+
+void IankoCar::set_body_position(float32 x, float32 y) {
+	body_descriptor.position.Set(x, y);
+}
+
+void IankoCar::set_wheel_position(float32 x, float32 y) {
+	wheel_descriptor.position.Set(x, y);
+}
+
+void IankoCar::init_wheel_structure() {
+
+	wheels_info.circle.m_radius = 0.4f;
+
+	wheels_info.phys_properties.shape = &wheels_info.circle;
+	wheels_info.phys_properties.density = 1.0f;
+	wheels_info.phys_properties.friction = 0.9f;
+}
+
 b2PolygonShape IankoCar::create_chassis()
 {
 	b2PolygonShape chassis;
@@ -22,5 +47,47 @@ void IankoCar::create_car_body() {
 	car_details.body = m_world->CreateBody(&bd);
 	m_car = m_world->CreateBody(&bd);
 	m_car->CreateFixture((car_details.chassis), 1.0f);
+
+}
+
+void IankoCar::joint_wheel(b2Body* wheel, const b2Vec2 & axis, const JointParams& params) {
+	wheel_joint_descriptor.Initialize(m_car, wheel, wheel->GetPosition(), axis);
+	
+	wheel_joint_descriptor.motorSpeed = params.motorSpeed;
+	wheel_joint_descriptor.maxMotorTorque = params.maxMotorTorque;
+	wheel_joint_descriptor.enableMotor = params.enableMotor;
+	wheel_joint_descriptor.frequencyHz = params.frequencyHz;
+	wheel_joint_descriptor.dampingRatio = params.dampingRatio;
+	//m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&jd);
+}
+
+void IankoCar::create_polygon(const float32 x_start) {
+
+	grnd_shape.Set({ -20.0f + x_start, 0.0f }, { 20.0f + x_start, 0.0f });
+	ground->CreateFixture(&grnd_properties); /*задать свойства*/
+	ground_end = 20.0f + x_start;
+
+				/*b2EdgeShape shape;
+			init_wheel_structure();
+			b2FixtureDef fd;
+			fd.shape = &shape;
+			fd.density = 0.0f;
+			fd.friction = 0.6f;
+
+			shape.Set(b2Vec2(-20.0f, 0.0f), b2Vec2(20.0f, 0.0f));
+			ground->CreateFixture(&fd);
+
+			float32 hs[10] = { 0.25f, 1.0f, 4.0f, 0.0f, 0.0f, -1.0f, -2.0f, -2.0f, -1.25f, 0.0f };
+
+			float32 x = 20.0f, y1 = 0.0f, dx = 5.0f;
+
+			for (int32 i = 0; i < 10; ++i)
+			{
+				float32 y2 = hs[i];
+				shape.Set(b2Vec2(x, y1), b2Vec2(x + dx, y2));
+				ground->CreateFixture(&fd);
+				y1 = y2;
+				x += dx;
+			}*/
 
 }
--- Car game/IankoPavelCar.h
+++ Car game/IankoPavelCar.h
@@ -7,24 +7,39 @@
 #ifndef IANKOCAR_H
 #define IANKOCAR_H
 
+struct WheelStructure {
+	b2CircleShape circle;
+	b2FixtureDef phys_properties;
+};
 
+struct JointParams {
+	float32 motorSpeed;
+	float32 maxMotorTorque;
+	bool enableMotor;
+	float32 frequencyHz;
+	float32 dampingRatio;
+};
 
 class IankoCar : public Test
 {
 public:
 	IankoCar()
 	{
-		m_hz = 4.0f;
-		m_zeta = 0.7f;
-		m_speed = 50.0f;
-
-		b2Body* ground = NULL;
+		//b2Body* ground = NULL;
 		{
 			b2BodyDef bd;
 			ground = m_world->CreateBody(&bd);
 
-			b2EdgeShape shape;
 
+			grnd_properties.shape = &grnd_shape;
+			grnd_properties.density = 0.0f;
+			grnd_properties.friction = 0.6f;
+			
+			
+			create_polygon(0.0f);
+
+			/*b2EdgeShape shape;
+			init_wheel_structure();
 			b2FixtureDef fd;
 			fd.shape = &shape;
 			fd.density = 0.0f;
@@ -72,143 +87,125 @@ public:
 
 			x += 40.0f;
 			shape.Set(b2Vec2(x, 0.0f), b2Vec2(x, 20.0f));
-			ground->CreateFixture(&fd);
+			ground->CreateFixture(&fd);*/
 		}
 
-		// Teeter
-		{
-			b2BodyDef bd;
-			bd.position.Set(140.0f, 1.0f);
-			bd.type = b2_dynamicBody;
-			b2Body* body = m_world->CreateBody(&bd);
+	//	// Teeter
+	//	{
+	//		b2BodyDef bd;
+	//		bd.position.Set(140.0f, 1.0f);
+	//		bd.type = b2_dynamicBody;
+	//		b2Body* body = m_world->CreateBody(&bd);
 
-			b2PolygonShape box;
-			box.SetAsBox(10.0f, 0.25f);
-			body->CreateFixture(&box, 1.0f);
+	//		b2PolygonShape box;
+	//		box.SetAsBox(10.0f, 0.25f);
+	//		body->CreateFixture(&box, 1.0f);
 
-			b2RevoluteJointDef jd;
-			jd.Initialize(ground, body, body->GetPosition());
-			jd.lowerAngle = -8.0f * b2_pi / 180.0f;
-			jd.upperAngle = 8.0f * b2_pi / 180.0f;
-			jd.enableLimit = true;
-			m_world->CreateJoint(&jd);
+	//		b2RevoluteJointDef jd;
+	//		jd.Initialize(ground, body, body->GetPosition());
+	//		jd.lowerAngle = -8.0f * b2_pi / 180.0f;
+	//		jd.upperAngle = 8.0f * b2_pi / 180.0f;
+	//		jd.enableLimit = true;
+	//		m_world->CreateJoint(&jd);
 
-			body->ApplyAngularImpulse(100.0f, true);
-		}
+	//		body->ApplyAngularImpulse(100.0f, true);
+	//	}
 
-		// Bridge
-		{
-			int32 N = 20;
-			b2PolygonShape shape;
-			shape.SetAsBox(1.0f, 0.125f);
+	//	// Bridge
+	//	{
+	//		int32 N = 20;
+	//		b2PolygonShape shape;
+	//		shape.SetAsBox(1.0f, 0.125f);
 
-			b2FixtureDef fd;
-			fd.shape = &shape;
-			fd.density = 1.0f;
-			fd.friction = 0.6f;
+	//		b2FixtureDef fd;
+	//		fd.shape = &shape;
+	//		fd.density = 1.0f;
+	//		fd.friction = 0.6f;
 
-			b2RevoluteJointDef jd;
+	//		b2RevoluteJointDef jd;
 
-			b2Body* prevBody = ground;
-			for (int32 i = 0; i < N; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position.Set(161.0f + 2.0f * i, -0.125f);
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&fd);
+	//		b2Body* prevBody = ground;
+	//		for (int32 i = 0; i < N; ++i)
+	//		{
+	//			b2BodyDef bd;
+	//			bd.type = b2_dynamicBody;
+	//			bd.position.Set(161.0f + 2.0f * i, -0.125f);
+	//			b2Body* body = m_world->CreateBody(&bd);
+	//			body->CreateFixture(&fd);
 
-				b2Vec2 anchor(160.0f + 2.0f * i, -0.125f);
-				jd.Initialize(prevBody, body, anchor);
-				m_world->CreateJoint(&jd);
+	//			b2Vec2 anchor(160.0f + 2.0f * i, -0.125f);
+	//			jd.Initialize(prevBody, body, anchor);
+	//			m_world->CreateJoint(&jd);
 
-				prevBody = body;
-			}
+	//			prevBody = body;
+	//		}
 
-			b2Vec2 anchor(160.0f + 2.0f * N, -0.125f);
-			jd.Initialize(prevBody, ground, anchor);
-			m_world->CreateJoint(&jd);
-		}
+	//		b2Vec2 anchor(160.0f + 2.0f * N, -0.125f);
+	//		jd.Initialize(prevBody, ground, anchor);
+	//		m_world->CreateJoint(&jd);
+	//	}
 
-		// Boxes
-		{
-			b2PolygonShape box;
-			box.SetAsBox(0.5f, 0.5f);
+	//	// Boxes
+	//	{
+	//		b2PolygonShape box;
+	//		box.SetAsBox(0.5f, 0.5f);
 
-			b2Body* body = NULL;
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
+	//		b2Body* body = NULL;
+	//		b2BodyDef bd;
+	//		bd.type = b2_dynamicBody;
 
-			bd.position.Set(230.0f, 0.5f);
-			body = m_world->CreateBody(&bd);
-			body->CreateFixture(&box, 0.5f);
+	//		bd.position.Set(230.0f, 0.5f);
+	//		body = m_world->CreateBody(&bd);
+	//		body->CreateFixture(&box, 0.5f);
 
-			bd.position.Set(230.0f, 1.5f);
-			body = m_world->CreateBody(&bd);
-			body->CreateFixture(&box, 0.5f);
+	//		bd.position.Set(230.0f, 1.5f);
+	//		body = m_world->CreateBody(&bd);
+	//		body->CreateFixture(&box, 0.5f);
 
-			bd.position.Set(230.0f, 2.5f);
-			body = m_world->CreateBody(&bd);
-			body->CreateFixture(&box, 0.5f);
+	//		bd.position.Set(230.0f, 2.5f);
+	//		body = m_world->CreateBody(&bd);
+	//		body->CreateFixture(&box, 0.5f);
 
-			bd.position.Set(230.0f, 3.5f);
-			body = m_world->CreateBody(&bd);
-			body->CreateFixture(&box, 0.5f);
+	//		bd.position.Set(230.0f, 3.5f);
+	//		body = m_world->CreateBody(&bd);
+	//		body->CreateFixture(&box, 0.5f);
 
-			bd.position.Set(230.0f, 4.5f);
-			body = m_world->CreateBody(&bd);
-			body->CreateFixture(&box, 0.5f);
-		}
+	//		bd.position.Set(230.0f, 4.5f);
+	//		body = m_world->CreateBody(&bd);
+	//		body->CreateFixture(&box, 0.5f);
+	//	}
 
 		// Car
 		{
+			init_wheel_structure();
 			b2PolygonShape chassis = create_chassis();
 
-			b2CircleShape circle;
-			circle.m_radius = 0.4f;
 
-
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(0.0f, 1.0f);
-
-			//create_car_body();
+			set_body_type(b2_dynamicBody);
+			set_wheel_type(b2_dynamicBody);
+			
 			Wheel* wh = builder.get_wheel(-1.0f, 0.35f);
 
-			m_car = m_world->CreateBody(&bd);
+			set_body_position(0.0f, 1.0f);
+			m_car = m_world->CreateBody(&body_descriptor);
 			m_car->CreateFixture(&chassis, 1.0f);
 
-			b2FixtureDef fd;
-			fd.shape = &circle;
-			fd.density = 1.0f;
-			fd.friction = 0.9f;
 
-			bd.position.Set(-1.0f, 0.35f);
-			m_wheel1 = m_world->CreateBody(&bd);
-			m_wheel1->CreateFixture(&fd);
+			set_wheel_position(-1.0f, 0.35f);
+			m_wheel1 = m_world->CreateBody(&wheel_descriptor);
+			m_wheel1->CreateFixture(&wheels_info.phys_properties);
 
-			bd.position.Set(1.0f, 0.4f);
-			m_wheel2 = m_world->CreateBody(&bd);
-			m_wheel2->CreateFixture(&fd);
+			set_wheel_position(1.0f, 0.4f);
+			m_wheel2 = m_world->CreateBody(&wheel_descriptor);
+			m_wheel2->CreateFixture(&wheels_info.phys_properties);
 
-			b2WheelJointDef jd;
 			b2Vec2 axis(0.0f, 1.0f);
 
-			jd.Initialize(m_car, m_wheel1, m_wheel1->GetPosition(), axis);
-			jd.motorSpeed = 0.0f;
-			jd.maxMotorTorque = 20.0f;
-			jd.enableMotor = true;
-			jd.frequencyHz = m_hz;
-			jd.dampingRatio = m_zeta;
-			m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&jd);
+			joint_wheel(m_wheel1, axis, left_wheel_characteristics);
+			m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&wheel_joint_descriptor);
 
-			jd.Initialize(m_car, m_wheel2, m_wheel2->GetPosition(), axis);
-			jd.motorSpeed = 0.0f;
-			jd.maxMotorTorque = 10.0f;
-			jd.enableMotor = false;
-			jd.frequencyHz = m_hz;
-			jd.dampingRatio = m_zeta;
-			m_spring2 = (b2WheelJoint*)m_world->CreateJoint(&jd);
+			joint_wheel(m_wheel2, axis, right_wheel_characteristics);
+			m_spring2 = (b2WheelJoint*)m_world->CreateJoint(&wheel_joint_descriptor);
 		}
 	}
 
@@ -249,7 +246,18 @@ public:
 		g_debugDraw.DrawString(5, m_textLine, "frequency = %g hz, damping ratio = %g", m_hz, m_zeta);
 		m_textLine += DRAW_STRING_NEW_LINE;
 
-		g_camera.m_center.x = m_car->GetPosition().x;
+		float32 car_x = m_car->GetPosition().x;
+		float32 car_y = m_car->GetPosition().y;
+
+		g_camera.m_center.x = car_x;//m_car->GetPosition().x;
+		g_camera.m_center.y = car_y;//m_car->GetPosition().y;
+
+		/*create further landscape if close to the end*/
+		float32 distance_to_see = 10.0f;
+		if (fabs(ground_end - car_x) < distance_to_see) {
+			create_polygon(car_x);
+		}
+
 		Test::Step(settings);
 	}
 
@@ -260,13 +268,19 @@ public:
 
 	CarDetails car_details{};
 
+	b2Body* ground = NULL;
+	b2EdgeShape grnd_shape;
+	b2FixtureDef grnd_properties;
+	float32 ground_end{};
+
 	b2Body* m_car;
 	b2Body* m_wheel1;
 	b2Body* m_wheel2;
 
-	float32 m_hz;
-	float32 m_zeta;
-	float32 m_speed;
+	float32 m_hz = 4.0f;
+	float32 m_zeta = 0.7f;
+	float32 m_speed = 50.0f;
+
 	b2WheelJoint* m_spring1;
 	b2WheelJoint* m_spring2;
 
@@ -275,10 +289,43 @@ public:
 		const float32 jump_intensity_y = 0.05f;
 		const float32 jump_intensity_x = 0.0f;
 		
+		b2BodyDef body_descriptor{};
+		b2BodyDef wheel_descriptor{};
+		b2WheelJointDef wheel_joint_descriptor{};
+
+		WheelStructure wheels_info{};
+		//b2FixtureDef wheel_structure{};
+
+		void set_body_type(b2BodyType type_);
+		void set_wheel_type(b2BodyType type_);
+
+		void set_body_position(float32 x, float32 y);
+		void set_wheel_position(float32 x, float32 y);
+		void init_wheel_structure();
+
 		b2PolygonShape create_chassis();
 		void create_car_body();
+		void joint_wheel(b2Body* wheel, const b2Vec2 & axis, const JointParams& params);
+		void create_polygon(const float32 x_start);
 
 		CarBuilder builder;
+
+		const JointParams left_wheel_characteristics
+		{
+			0.0f,
+			20.0f,
+			true,
+			m_hz,
+			m_zeta
+		};
+		const JointParams right_wheel_characteristics
+		{
+			0.0f,
+			10.0f,
+			false,
+			m_hz,
+			m_zeta
+		};
 };
 
 #endif
